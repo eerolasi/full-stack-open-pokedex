3b45abe0a346ad4f8de72a4fce993fae70c24890
eerolasi <93593718+eerolasi@users.noreply.github.com>
2024-07-15 18:56:12 +0300

    add pokemon page test

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     supportFile: false,
+    baseUrl: 'http://localhost:5001',
   },
 })
--- cypress/e2e/pokedox.cy.js
+++ cypress/e2e/pokedox.cy.js
@@ -1,9 +1,14 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
-    cy.visit('http://localhost:5001')
+    cy.visit('')
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
+  })
+  it('pokemon page can be navigated to', function () {
+    cy.visit('/')
+    cy.contains('bulbasaur').click()
+    cy.contains('chlorophyll')
   })
 })
