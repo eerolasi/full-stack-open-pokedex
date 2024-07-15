0d2a220f1db239d8dc4349fc8f1a947ee93fd57d
eerolasi <93593718+eerolasi@users.noreply.github.com>
2024-07-15 13:13:40 +0300

    fix lint

--- app.js
+++ app.js
@@ -7,5 +7,6 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
+  /* eslint-disable no-console */
   console.log(`server started on port ${PORT}`)
 })
